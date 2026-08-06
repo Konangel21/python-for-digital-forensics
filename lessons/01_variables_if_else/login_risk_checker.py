0015c79064fb67adba8f481b46952faaa92dc8f4
Konangelsharma <konangelsharma@gmail.com>
2026-08-06 18:11:49 +0530

    Add login risk checker functionality

--- lessons/01_variables_if_else/login_risk_checker.py
+++ lessons/01_variables_if_else/login_risk_checker.py
@@ -1,1 +1,13 @@
+Username = input("Enter Username: ")
+Ip_address = input("Enter IP address: ")
+Failed_attempts = int(input("Enter number of Failed attempts: "))
+if Failed_attempts >=3:
+    Status = "Suspicious"
+else:
+    Status = "Normal"
 
+print("===== Login Report =====")
+print("Username:", Username)
+print("Ip_address:", Ip_address)
+print("Failed_attempts:", Failed_attempts)
+print("Status:", Status)
